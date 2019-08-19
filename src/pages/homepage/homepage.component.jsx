fcfeefdfb1696031803eff576909506981b0059e
theblackcrown <digidi9@mail.com>
2019-08-19 20:47:15 +0300

    we added image container and breaked items into component and style them

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../components/directory/directory.component';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory />
+    </div>
+)
+
+export default HomePage;
