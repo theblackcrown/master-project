1a2dee04f3813e8f2146d07b03558a1761546812
theblackcrown <digidi9@mail.com>
2019-08-21 21:03:15 +0300

    we created sign up page and component and we store all in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,32 @@ const config = {
     storageBucket: "",
     messagingSenderId: "144714433290",
     appId: "1:144714433290:web:3b74f4a3578d1750"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating useru', error.message);
+
+        }
+    }
+    return userRef;
 }
 
 firebase.initializeApp(config);
