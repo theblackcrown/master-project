046d4c86c6466a2ea2592b0f01a97f30443a2a7b
theblackcrown <digidi9@mail.com>
2019-08-23 18:08:27 +0300

    we added cart to the page and quantity base on when we use it

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,7 +1,8 @@
 
 
 const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: 'TOGGLE_CART-HIDDEN'
+    TOGGLE_CART_HIDDEN: 'TOGGLE_CART-HIDDEN',
+    ADD_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;
