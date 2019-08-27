b962c7ea4da6ac9fa80a7edafdce7b709c1d9799
theblackcrown <digidi9@mail.com>
2019-08-27 16:03:24 +0300

    we romove the creat logger for the development mode

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewars = [logger];
+const middlewars = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewars.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewars));
 
