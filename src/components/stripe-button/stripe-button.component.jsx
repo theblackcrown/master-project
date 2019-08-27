8a035b5ce2c3726b986dd808f11eb0b25ef89676
theblackcrown <digidi9@mail.com>
2019-08-27 13:06:38 +0300

    we add payment method with strip and changing out title from rect app to crwn clothing

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_5Ba1fjFDRC403aiVMDOjTjmC00Dz3EfSNG';
+
+    const onToken = token => {
+        console.log(token);
+
+        alert('payment successful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name="CRWN Clothing ins."
+            billingAddress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -3,11 +3,12 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
-import CartItem from '../../components/cart-item/cart-item.component';
+
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className="checkout-page">
@@ -37,6 +38,12 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className="total">
             <span>TOTAL: ${total}</span>
         </div>
+        <div className="test-warning">
+            *Please use the following test credit card for payment*
+            <br />
+            4242 4242 4242 4242 - EXP: 01/20 - CVV: 123
+        </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
